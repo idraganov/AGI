c1bcdb56f1dffe1509fa35f349ba5abd43b6cc8b
Draganov <draganov@gmail.com>
2018-09-22 17:27:59 +0300

    Added a few additional commands

--- git.py
+++ git.py
@@ -1,11 +1,12 @@
 
-### Training workshop (Git) - 15 min ###
+### Training workshop (Git) - 40 min ###
 
 git config --global user.name "Name"
 git config --global user.email "Email"
 
 git --version # git version
-git init # initializes a brand new Git repository
+git init my_first_repository # initializes a brand new Git repository
+ls -a # list all files, including the hidden ones
 git clone "link" # clones to local
 start notepad++ "filename"
 git add "filename" # adds a file to staging
@@ -16,11 +17,18 @@ git add *.py
 git commit -m "some message" # save a version
 
 git commit -am "some message" # in one step add and commit
+git commit -a -m "some message"
+
+git checkout "XXXX" # usually the first 5/6 characters are enough
+cat "filename" # to read the contents of a file
+git checkout master # to go back to the latest branch message
 
 git status # shows the current status of repository
 git push # origin master - send changes to the repository
 git pull # pull from repository to my PC
 git log # log of all commits (history)
+
+git diff "XXXXX 1" "XXXXX 2" # see the changes between 2 commits
 
 git reset --hard <commit hash for the desired version> # go back to a previous changes
 git reset --hard origin/master # to use the cloud version
@@ -44,8 +52,9 @@ git branch -D <branch name> # delete the branch
 touch "filename.txt" # create new file
 touch .gitignore # add the file name that you don't want to include
 git remote # list the remote repositories
-git remote add origin https://... #add remote repository
+git remote add origin https://... # add remote repository
 git push -u origin master
+mv my_repository my_new_repository # renaming a project
 
 Links:
 https://guides.github.com/introduction/git-handbook/
